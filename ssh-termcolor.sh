1ddb469239aabba10aa56fe3e694f178f6790ed7
angelo <angelo@fluxton.co.uk>
2019-07-15 11:40:03 +0100

    ssh-termcolor

--- ssh-termcolor.sh
+++ ssh-termcolor.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
   if [ "$1" == "mickey" ]; then
-    # printf '\033]11;#231C1F\007'
+    printf '\033]11;#332C1F\007'
+  elif [ "$1" == "think" ]; then
     printf '\033]11;#332C1F\007'
   elif [ "$1" == "drip" ]; then
-    printf '\033]11;#292476\007'
+    printf '\033]11;#292446\007'
+  elif [ "$1" == "shell.gridhost.co.uk" ]; then
+    printf '\033]11;#492426\007'
   elif [ "$1" == "other" ]; then
     printf '\033]11;#2533f0\007'
   else
